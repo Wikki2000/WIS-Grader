ffdece89166c792fec8c30d27759d2fb47e9a38b
Idris Elgarrab <franckenstein999@gmail.com>
2024-08-24 14:11:22 -0700

    upload files

---
+++ models/course.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Course model implementation using SQLAlchemy."""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import Base, BaseModel
+from sqlalchemy.orm import relationship
+from models.result import Result
+
+
+class Course(BaseModel, Base):
+    """Course class represents a course in the grading system."""
+
+    __tablename__ = 'courses'
+
+    course_title = Column(String(100), nullable=False)
+    course_code = Column(String(20), nullable=False, unique=True)
+    credit_load = Column(Integer, nullable=False)
+    semester = Column(String(20), nullable=False)
+    lecturer_id = Column(String(60), ForeignKey('lecturers.id'))
+
+    results = relationship('Result', backref='course', cascade='all, delete-orphan')
