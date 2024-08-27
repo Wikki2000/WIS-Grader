33a8736216030c1a2815ec9eaad42169a21a8217
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-08-28 00:08:22 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
 
-from api.v1 import app
+from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flasgger import Swagger
+from api.v1.views import app_views
+from .config import Config
+from models.storage import Storage
+
+# Initialize storage
+storage = Storage()
+
+# Initialize Flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Initialize extensions
+jwt = JWTManager(app)
+swagger = Swagger(app)
+
+# Allow cross-origin requests from port 5000
+cors = CORS(app, resources={r"/*": {"origins": "http://localhost:5000"}})
+
+# Register blueprint
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 if __name__ == '__main__':
