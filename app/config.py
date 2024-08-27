86e286a95aa40f16aa9b05c001b81e3d97158cc6
wikki2000 <wisdomokposin@gmail.com>
2024-08-27 23:17:41 +0100

    Set up flask appliaction

---
+++ app/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines configuration settings for the Flask application.
+
+The `Config` class serves as the base configuration, providing default settings
+that can be inherited and overridden by environment-specific configurations.
+
+Key configurations include:
+
+- SECRET_KEY: Use for session management and other security-related features.
+- SWAGGER: Use for Swagger UI, including the title and UI version.
+"""
+from os import environ
+
+
+class Config:
+    """Base configuration."""
+    SECRET_KEY = getenv("FLASK_SECRET_KEY")
