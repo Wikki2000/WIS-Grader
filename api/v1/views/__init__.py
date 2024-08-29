90701503ddbce4c3ddc5b98bf2f431eff9e9eec4
wikki2000 <wisdomokposin@gmail.com>
2024-08-29 22:56:53 +0100

    Add deleted file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,9 +5,4 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__)
 
 from api.v1.views.auth.login import *
-from .courses import *
-from .enrollments import *
-from .grades import *
-from .lecturers import *
-from .students import *
-from .users import *
+from api.v1.views.index import *
