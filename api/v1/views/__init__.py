3f38a09651e94f407f89055a90ca19b06d36dc17
wikki2000 <wisdomokposin@gmail.com>
2024-08-31 13:47:31 +0100

    Merge auth_register to reviewed_auth

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+"""Create the blueprint of flask"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__)
 
+from api.v1.views.auth.send_token import *
 from api.v1.views.auth.register import *
 from api.v1.views.index import *
