5a472446a5e91116e92fe37785e45f36d2b78907
wikki2000 <wisdomokposin@gmail.com>
2024-08-31 14:21:01 +0100

    Merge auth_login branch with reviewed_auth

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__)
 
+from api.v1.views.auth.login import *
 from api.v1.views.auth.send_token import *
 from api.v1.views.auth.register import *
 from api.v1.views.index import *
