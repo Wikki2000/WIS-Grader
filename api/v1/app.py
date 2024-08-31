0a73f89b31eed51923f2b7416486d09dc91382ef
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-08-31 15:02:22 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Setuo Flask Appplication."""
+"""Create Flask Appplication."""
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
