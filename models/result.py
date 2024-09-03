4a545e74e40602488e88bf63538ea3e740ee6406
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-09-03 12:27:36 +0100

    Update result.py

--- models/result.py
+++ models/result.py
@@ -15,4 +15,4 @@ class Result(BaseModel, Base):
     grade = Column(String(10))
     remark = Column(Integer)
     course_id = Column(String(60), ForeignKey('courses.id', ondelete='CASCADE'), nullable=False)
-    student_id = Column(String(60), ForeignKey('students.id', ondelete='CASCADE), nullable=False)
+    student_id = Column(String(60), ForeignKey('students.id', ondelete='CASCADE'), nullable=False)
