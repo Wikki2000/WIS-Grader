e2cd8a61e457ea9ba52fcea8992c0b1954f59168
wikki2000 <wisdomokposin@gmail.com>
2024-09-04 23:45:48 +0100

    Update the /signup route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,82 +4,63 @@ from app.routes import app
 from flask import render_template, request, redirect, url_for, jsonify
 from flask import flash, session
 import requests
+from uuid import uuid4
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/account/signup', methods=['GET', 'POST'])
 def register():
     """Handle user registration."""
     if request.method == 'GET':
-        return render_template('register.html')
+        return render_template('register.html', cache_id=uuid4())
 
-    # Process the form data
-    firstname = request.form['firstname']
-    lastname = request.form['lastname']
-    email = request.form['email']
-    password = request.form['password']
-    confirm_password = request.form['confirm_password']
+    data = request.get_json()
 
-    # Logic to handle registration
-    if password == confirm_password:
-        # Save data in the session
-        session['registration_data'] = {
-            'firstname': firstname,
-            'lastname': lastname,
-            'email': email,
-            'password': password
-        }
+    # This handle missing field that may be sent from curl
+    required_fields = ["firstname", "lastname", "email", "password"]
+    for field in required_fields:
+        if field not in data:
+            abort(400, "Bad Request")
 
-        # Retrieve email and name from session
-        registration_data = session.get('registration_data')
-        if not registration_data:
-            flash("Error: No registration data found")
-            return redirect(url_for('app.register'))
+    firstname = data.get('firstname')
+    lastname = data.get('lastname')
+    email = data.get('email')
+    password = data.get('password')
 
-        email = registration_data['email']
-        firstname = registration_data['firstname']
-        lastname = registration_data['lastname']
+    # Save data in the session
+    session['registration_data'] = {
+        'firstname': firstname,
+        'lastname': lastname,
+        'email': email,
+        'password': password
+    }
 
-        # Send verification token
-        url = 'http://127.0.0.1:5001/api/v1/auth/send-token'
-        res = requests.post(
-            url,
-            json={'email': email, 'name': f'{firstname} {lastname}'}
-        )
-        token = res.json().get('token')
+    # Send verification token
+    url = 'http://127.0.0.1:5001/api/v1/auth/send-token'
+    res = requests.post(
+        url,
+        json={'email': email, 'name': f'{firstname} {lastname}'}
+    )
+    token = res.json().get('token')
 
-        if token:
-            print(token)
-            # Redirect to email verification route
-            return redirect(url_for('app.verify_email'))
-        else:
-            flash("Error: Could not send verification email")
-            return redirect(url_for('app.register'))
-
-    flash("Error: Passwords do not match")
-    return render_template('register.html')
+    if token:
+        return jsonify({'status': 'Success'}), 200
+    return jsonify({'error': 'Internal Error Occured'}), 500
 
 
-@app.route('/verify_email', methods=['GET', 'POST'])
+
+@app.route('/account/verify-email', methods=['GET', 'POST'])
 def verify_email():
     """Verify the email using a token."""
     if request.method == 'GET':
-        return render_template('verify_email.html')
+        email = request.args.get("email")
+        data = {'email': email, 'cache_id': uuid4()}
+        return render_template('verify_email.html', **data)
 
-    # Process the form data
-    f1 = request.form.get('f1')
-    f2 = request.form.get('f2')
-    f3 = request.form.get('f3')
-    f4 = request.form.get('f4')
-    f5 = request.form.get('f5')
-    f6 = request.form.get('f6')
 
-    # Combine inputs into a single token
-    token = f1 + f2 + f3 + f4 + f5 + f6
 
     # Retrieve registration data from session
     registration_data = session.get('registration_data')
     if not registration_data:
-        flash("Error: No registration data found")
         return redirect(url_for('app.register'))
 
     email = registration_data['email']
@@ -90,15 +71,15 @@ def verify_email():
     # Send token and registration data to the API
     url = 'http://127.0.0.1:5001/api/v1/auth/register'
     response = requests.post(
-        url,
-        json={
-            'email': email,
-            'first_name': firstname,
-            'last_name': lastname,
-            'password': password,
-            'token': token
-        }
-    )
+            url,
+            json={
+                'email': email,
+                'first_name': firstname,
+                'last_name': lastname,
+                'password': password,
+                'token': token
+                }
+            )
 
     if response.status_code == 200:
         return render_template('confirm_email.html')
