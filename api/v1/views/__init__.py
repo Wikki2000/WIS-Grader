f2c622a66c4ecf7b5d4d87684f9675b133837ea0
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-09-04 16:05:01 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,5 +7,4 @@ app_views = Blueprint('app_views', __name__)
 from api.v1.views.auth.login import *
 from api.v1.views.auth.send_token import *
 from api.v1.views.auth.register import *
-from api.v1.views.results import *
 from api.v1.views.index import *
