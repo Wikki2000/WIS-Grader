9d6c98397a0555963c5b8707906a23eb7367e208
Idris Elgarrab <franckenstein999@gmail.com>
2024-09-06 22:41:02 +0100

    update(PEP8): Make the file PEP8 compatible

--- app/config.py
+++ app/config.py
@@ -14,10 +14,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     """Base configuration."""
     SECRET_KEY = environ["FLASK_SECRET_KEY"]
     JWT_SECRET_KEY = environ['JWT_SECRET_KEY']
     JWT_TOKEN_LOCATION = ['cookies']
-    JWT_ACCESS_COOKIE_PATH = '/'      # Specify the cookie path
     JWT_COOKIE_SECURE = True          # Only transmit cookies over HTTPS
