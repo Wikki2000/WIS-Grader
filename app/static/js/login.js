d4adb90b8587ca62ac4e3c5a23daddd7cfb2e9cb
Idris Elgarrab <franckenstein999@gmail.com>
2024-09-06 17:19:26 +0100

    update

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,15 +1,13 @@
-import { ajaxRequest } from './utils.js'
+import { ajaxRequest } from './utils.js';
 
 $(document).ready(function () {
 
-  // Handle login form submission
   $('.register__form').submit(function (event) {
     event.preventDefault();
 
     const email = $('#email').val();
     const password = $('#password').val();
 
-    // Check if email and password are not empty
     if (!email || !password) {
       alert("Email and password are required.");
       return;
@@ -20,20 +18,16 @@ $(document).ready(function () {
       password: password
     });
 
-    const url = 'http://127.0.0.1:5000/account/signin';
-
-    ajaxRequest(url, "POST", data,
-      (response) => {
+    ajaxRequest('http://127.0.0.1:5000/account/signin', 'POST', data,
+      function (response) {
         if (response.message === "Login Successful") {
-          // Redirect to the dashboard after successful login
-          window.location.href = 'http://127.0.0.1:5000/dashboard';  // Redirect to the dashboard
+          window.location.href = 'http://127.0.0.1:5000/dashboard';
         } else {
           alert("Invalid Email or Password");
         }
       },
-      (error) => {
-        console.error('An error occurred during login', error);
-        alert("Invalid Email or Password");
+      function (error) {
+        alert("An error occurred during login");
       }
     );
   });
