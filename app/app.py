f9016705c422e0388b4ac58443a4bf705fd6eb56
Idris Elgarrab <franckenstein999@gmail.com>
2024-09-06 17:18:19 +0100

    add jwt config

--- app/app.py
+++ app/app.py
@@ -5,13 +5,19 @@ from flask import Flask, jsonify
 from app.routes import app as bp
 import sys
 import os
+from flask_jwt_extended import JWTManager
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 # Setting up Flask Application.
 app = Flask(__name__)
 app.config.from_object(Config)
-
+app.config['JWT_TOKEN_LOCATION'] = ['cookies'] 
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/'  # Specify the cookie path
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False  # Optionally disable CSRF protection for development
+app.config['JWT_COOKIE_SECURE'] = True  # Only transmit cookies over HTTPS
+# Initialize the JWT Manager
+jwt = JWTManager(app)
 
 # Register blueprint
 app.register_blueprint(bp)
