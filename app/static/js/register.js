0ca606f3c1423409504b149ad62ff29626f12472
Solomon Nwante <myphilothea@gmail.com>
2024-09-07 13:25:19 +0100

    style: Update alert styling

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -4,6 +4,7 @@ import { ajaxRequest, alertBox } from './utils.js';
 $(document).ready(function () {
 
   const alertDivClass = 'auth__alert__msg';
+  const pwd_input_border = $('input[type="password"]');
 
   // Ensure password match and meet some criteria.
   $('.signup__btn').click(function (event) {
@@ -14,11 +15,12 @@ $(document).ready(function () {
 
     if (pwd1 !== pwd2) {
       event.preventDefault();
-      const msg = 'Password Must Match';
+      const msg = 'Password must match';
       alertBox(alertDivClass, msg);
+      pwd_input_border.addClass('highlight__pwd__input');
     } else if (!passwordPattern.test(pwd1)) {
       event.preventDefault();
-      const msg = 'Password must be atleast 8 characters and ' + 
+      const msg = 'Password must be atleast 8 characters and ' +
                   'contains uper, lowercase and special character';
       alertBox(alertDivClass, msg);
     }
@@ -38,7 +40,7 @@ $(document).ready(function () {
     const firstName = $('#firstname').val();
     const lastName = $('#lastname').val();
     const email = $('#email').val();
-    const password = $('#password').val() 
+    const password = $('#password').val()
 
     const data = JSON.stringify({
       firstname: firstName, email: email,
@@ -59,7 +61,7 @@ $(document).ready(function () {
          }
       },
       (error) => {
-        const msg = 'User With This Email Exists Already';
+        const msg = 'This user already exist';
         alertBox(alertDivClass, msg);
 
         // Hide loader and display button to user on error
