000a6ecd96c9059a54d993daf7a5eff59f7f06a9
wikki2000 <wisdomokposin@gmail.com>
2024-09-15 08:51:23 +0100

    Update: course_management.js

--- app/static/js/course_management.js
+++ app/static/js/course_management.js
@@ -5,20 +5,21 @@ $(document).ready(function () {
   // Define course managemnt API globally
   const courseEndpoint = '/courses';
 
+
   /* =============== GET REQUEST ================*/
   $.get(courseEndpoint)
     .done(response => {
       if (response.status === 'Success') {
         const courses = response.courses;
         $.each(courses, (index, course) => {
-          const newCourse = `<tr id="course_${course.course_code}">
+          const newCourse = `<tr id="course_${course.id}">
             <td><i class="fa fa-book" aria-hidden="true"></i></td>
             <td>${course.course_code}</td>
             <td>${course.course_title}</td>
             <td>${course.credit_load}</td>
-            <td>50 (dummy data for now)</td>
-            <td><button class="edit-btn" data-id="${course.id}"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</button></td>
-            <td><button class="delete-btn" data-id="${course.id}"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
+	    <td>${course.student_count}</td>
+            <td><button class="edit__btn" data-id="${course.id}"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</button></td>
+            <td><button class="delete__btn" data-id="${course.id}"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
           </tr>`
           $('table tbody').append(newCourse);
         });
@@ -30,8 +31,9 @@ $(document).ready(function () {
       console.log('Error thrown:', errorThrown);
     });
 
+
   /* =============== POST REQUEST ================*/
-  $('.course__management__form').submit(function (event) {
+  $('.popup__modal').on('submit', '.course__management__form', function (event) {
 
     event.preventDefault();
 
@@ -52,17 +54,23 @@ $(document).ready(function () {
           const newCourse = `<tr id="course_${response.course.id}">
             <td><i class="fa fa-book" aria-hidden="true"></i></td>
             <td>${response.course.course_code}</td>
-            <td>${response.course.course_title}</td>
+            <td>${response.course.course_name}</td>
             <td>${response.course.credit_load}</td>
-            <td>50 (dummy data for now)</td>
-            <td><button class="edit-btn" data-id="${response.course.id}"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</button></td>
-            <td><button class="delete-btn" data-id="${response.course.id}"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
+            <td>${response.course.student_count}</td>
+            <td><button class="edit__btn" data-id="${response.course.id}"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</button></td>
+            <td><button class="delete__btn" data-id="${response.course.id}"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
           </tr>`
           $('table tbody').append(newCourse);
 
-          alert("Courses Create Successfully");
-          $('.course__modal').hide();
-          $('.course__management__form')[0].reset();
+          $('.course__modal').remove();
+
+          $('.popup__modal').load('/static/modal-course-added-success', function () {
+            $('.fa-times').click(function () {
+              $('.popup__modal').empty();
+            });
+
+          });
+
         }
       },
       (error) => {
@@ -75,11 +83,51 @@ $(document).ready(function () {
 
 
   /* =============== DELETE REQUEST ================*/
-  $('tbody').on('click', '.delete-btn', function () {
-    const course_id = $('.delete-btn').data('id');
-    ajaxRequest(`${courseEndpoint}/${course_id}`, "DELETE", null,
+  $('tbody').on('click', '.delete__btn', function () {
+    const courseId = $(this).data('id');
+
+    $('.popup__modal').load('/static/modal-confirm-delete', function () {
+
+      // Send request to delete after clicking delete button
+      $('.button--delete').click(function () {
+        ajaxRequest(`${courseEndpoint}/${courseId}`, 'DELETE', null,
+          (response) => {
+            if (response.status == 'Success') {
+
+              $('.popup__modal').load('/static/modal-success', function () {
+                const $popupModal = $(this);
+
+                $('P.modal__subtitle').append('Item deleted successfully!');
+
+                $('body').on('click', 'button.button--continue', function () {
+                  $popupModal.empty();
+                  $(`#course_${courseId}`).remove();
+                });
+
+              });
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      });
+
+      $('.button--cancel, .fa-times').click(function () {
+        $('.modal--delete').remove();
+      });
+
+    });
+
+  });
+
+
+  /* =============== PUT REQUEST ================*/
+/*
+  $('tbody').on('click', '.edit-btn', function () {
+    const course_id = $(this).data('id');
+    ajaxRequest(`${courseEndpoint}/${course_id}`, 'PUT', null,
       (response) => {
-        alert(`#course_${course_id}`);
         $(`#course_${course_id}`).remove();
         alert("deleted successfully");
       },
@@ -88,18 +136,40 @@ $(document).ready(function () {
       }
     );
   });
-
-
-  /* =============== PUT REQUEST ================*/
+*/
 
 
   /* =============== Program Functionality ================*/
-  $('.cancel').click(function () {
-    $('.course__modal').hide();
-  });
 
-  // Display pop up form on click
-  $('.add__course').click(function () {
-    $('.course__modal').show();
-  })
+  // Dynamically Load the Course Creation Form for POST and PUT request
+  $('body').on('click', '.add__course, .edit__btn', function () {
+
+    const $clickedBtn = $(this);
+
+    $('.popup__modal').load('/static/modal-course-form', function () {
+
+      // Update input field with value when Edit btn is pressed.
+      if ($clickedBtn.hasClass('edit__btn')) {
+        const courseId = $clickedBtn.data('id');
+        $.get(`${courseEndpoint}/${courseId}`)
+          .done(response => {
+            $('#course__code').val(response.course_code);
+            $('#course__name').val(response.course_title);
+            $('#credit__load').val(response.credit_load);
+            $('#course__description').val(response.description);
+
+            console.log(response);
+          })
+          .fail((jqXHR, textStatus, errorThrown) => {
+            console.log('Error status:', jqXHR.status);
+            console.log('Error thrown:', errorThrown);
+            console.log('Error text:', textStatus);
+          });
+      }
+
+      $('.cancel').click(function () {
+        $('.course__modal').remove();
+      });
+    });
+  });
 });
