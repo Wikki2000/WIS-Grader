c1998dbe8bb0a6c9dc75d52a68bec52aac48de45
Solomon Nwante <myphilothea@gmail.com>
2025-02-16 22:49:11 +0100

    Fix course summary table menu

--- app/static/js/global/templates.js
+++ app/static/js/global/templates.js
@@ -9,9 +9,9 @@
  * @return {String} - The course template.
  */
 export function courseListTableTemplate(data, date) {
-  const [code, year] = data.code.split('_');
+  const [code, year] = data.code.split("_");
   const row = `<tr>
-    <td class="">
+    <td class="course_name">
       <p>${data.name}</p>
     </td>
     <td class="">
@@ -24,37 +24,96 @@ export function courseListTableTemplate(data, date) {
     <td class="">
       <p class="">50</p>
     </td>
-    <td>
-      <p><i class="fa fa-ellipsis-v"></i></p>
-      <p><i style="display: none;" class="fa fa-times"></i></p>
-    </td>
-    <td class="manage">
-      <nav class="manage__nav">
-        <ul class="manage__list">
-          <li data-id="${data.id}" class="manage__item guest__list-bookDetail guest__listMenu">
-            <i class="fa fa-eye"></i>Details
-          </li>
-          <li data-id="${data.id}" class="manage__item guest__listEdit  guest__listMenu">
-            <i class="fa fa-edit"></i>Edit
-          </li>
-          <li data-id="${data.id}" class="manage__item guest__listPrint  guest__listMenu">
-            <i class="fa fa-share"></i>Share Link
-          </li>
-          <li data-id="${data.id}" class="manage__item guest__listPrint  guest__listMenu">
-            <i class="fa fa-user-plus"></i>Enroll Student
-          </li>
-          <li data-id="${data.id}" class="manage__item guest__listPrint  guest__listMenu">
-            <i class="fa fa-ban"></i>Stop Registration
-          </li>
-          <li data-id="${data.id}" class="manage__item guest__listPrint  guest__listMenu">
-            <i class="fa fa-tasks"></i>Allow Registration
-          </li>
-          <li data-id="${data.id}" class="manage__item guest__listPrint  guest__listMenu">
-            <i class="fa fa-trash"></i>Delete
-          </li>
-        </ul>
-      </nav>
+    <td class="menu_cell">
+      <button class="menu_toggle">⋮</button>
+      <ul class="menu_list">
+        <li data-id="${data.id}" class="menu_item guest__list-bookDetail guest__listMenu">
+          <i class="fa fa-eye"></i>Details
+        </li>
+        <li data-id="${data.id}" class="menu_item guest__listEdit  guest__listMenu">
+          <i class="fa fa-edit"></i>Edit
+        </li>
+        <li data-id="${data.id}" class="menu_item guest__listPrint  guest__listMenu">
+          <i class="fa fa-share"></i>Share
+        </li>
+        <li data-id="${data.id}" class="menu_item guest__listPrint  guest__listMenu">
+          <i class="fa fa-user-plus"></i>Enroll Student
+        </li>
+        <li data-id="${data.id}" class="menu_item guest__listPrint  guest__listMenu">
+          <i class="fa fa-ban"></i>Stop Registration
+        </li>
+        <li data-id="${data.id}" class="menu_item guest__listPrint  guest__listMenu">
+          <i class="fa fa-tasks"></i>Allow Registration
+        </li>
+        <li data-id="${data.id}" class="menu_item guest__listPrint  guest__listMenu">
+          <i class="fa fa-trash"></i>Delete
+        </li>
+      </ul>
     </td>
   </tr>`;
   return row;
 }
+
+const homeDashboardTable = document.getElementById("home_dashboard_table");
+
+homeDashboardTable.addEventListener("click", function (event) {
+  if (event.target.classList.contains("menu_toggle")) {
+    let menu = event.target.nextElementSibling;
+    let buttonRect = event.target.getBoundingClientRect();
+
+    let parentTd = event.target.closest("td"); // Get table cell
+    let parentRect = parentTd.getBoundingClientRect(); // Get table cell position
+
+    // Close any other open menus
+    homeDashboardTable.querySelectorAll(".menu_list").forEach((list) => {
+      if (list !== menu) {
+        list.style.display = "none";
+      }
+    });
+
+    // Toggle visibility
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+      return;
+    }
+
+    menu.style.display = "block";
+    menu.style.position = "absolute";
+    menu.style.visibility = "hidden";
+
+    let menuHeight = menu.offsetHeight;
+    let menuWidth = menu.offsetWidth;
+
+    // Position menu close to the button
+    let topPosition = buttonRect.bottom - parentRect.top - 20;
+    let rightPosition = parentRect.right - buttonRect.right + 50;
+
+    // Adjust if the menu overflows the table cell
+    if (topPosition + menuHeight > parentTd.clientHeight) {
+      topPosition = buttonRect.top - parentRect.top - menuHeight + 20; // Move above button
+    }
+
+    /*====== Please dont't remove =======
+    if (rightPosition + menuWidth > parentTd.clientWidth) {
+      rightPosition = parentTd.clientWidth - menuWidth; // Keep inside the cell
+    }
+    */
+
+    // Apply positioning
+    menu.style.top = `${topPosition}px`;
+    menu.style.right = `${rightPosition}px`;
+    menu.style.visibility = "visible";
+  }
+});
+
+// Hide menu when clicking outside
+document.addEventListener("click", function (event) {
+  document.querySelectorAll(".menu_list").forEach((menu) => {
+    if (
+      !menu.contains(event.target) &&
+      !event.target.classList.contains("menu_toggle")
+    ) {
+      menu.style.display = "none";
+    }
+  });
+});
